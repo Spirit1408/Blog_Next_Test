15eda45ecb142cf19005f3248dfb3a7b78e8b0e0
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-21 14:25:14 -0300

    Update multilingual approach

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ['en', 'uk'],
-        defaultLocale: 'en',
-        localeDetection: false,
-    }
+
 };
 
 export default nextConfig;
