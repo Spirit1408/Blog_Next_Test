1dd311a274ce8a0f2947f02f9d45e9b5dfa7688d
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-22 12:21:03 -0300

    Add root layout

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Mini Blog - Multilingual Next.js Blog",
+  description: "A multilingual blog built with Next.js featuring SSR, SSG, and i18n support",
+};
+
+export default function RootLayout({ children }) {
+  return children;
+}
